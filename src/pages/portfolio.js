b4a8f523ae616251c71a032d871b71b42fc13aac
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2019-12-15 15:13:54 -0500

    full width

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -3,26 +3,21 @@ import { graphql, Link } from 'gatsby'
 import Img from "gatsby-image"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import pageCSS from "./pageCSS.module.css"
+import "./portfolio.css"
 
 const PortfolioPage = ({ data }) => (
   <Layout>
-    <SEO title="Portfolio" keywords={[`gatsby`, `application`, `react`]} />
-    <ul className={pageCSS.postList}>
-      {data.allWordpressWpProject.edges.map(post => (
-        <li className={pageCSS.blogPost}>
-          <Link to={`/project/${post.node.slug}`} className={pageCSS.imageLink} >
-            <Img sizes={post.node.featured_media.localFile.childImageSharp.sizes} alt={post.node.title} className={pageCSS.postImage} />
+    <SEO title="Portfolio" keywords={[`Architecture`, `Grid`, `Portfolio`]} />
+
+    <div class="masonry">
+    {data.allWordpressWpProject.edges.map(post => (
+        <div className="masonry-item">
+          <Link to={`/project/${post.node.slug}`} className="masonry-item-link" >
+            <Img sizes={post.node.featured_media.localFile.childImageSharp.sizes} alt={post.node.title} className="masonry-item-img" />
           </Link>
-            <div className={pageCSS.postContent}>
-              <Link to={`/project/${post.node.slug}`} style={{ display: "flex", color: "black", textDecoration: "none" }} >
-                <h3 dangerouslySetInnerHTML={{ __html: post.node.title }} style={{ marginBottom: 0 }} />
-              </Link>
-              <div dangerouslySetInnerHTML={{ __html: post.node.excerpt }} />
-            </div>
-        </li>
+        </div>
       ))}
-    </ul>
+    </div>
   </Layout>
 )
 
