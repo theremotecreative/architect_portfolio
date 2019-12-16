fa5bc3f74832e4f7c2020008076afc530109a645
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2019-12-16 17:27:32 -0500

    css

--- src/templates/Page.js
+++ src/templates/Page.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout.js"
+import NarrowTemplate from "../components/NarrowTemplate"
 import SEO from "../components/seo"
 
 const PageTemplate = ({ data }) => (
@@ -9,8 +10,10 @@ const PageTemplate = ({ data }) => (
       title={data.wordpressPage.title}
       description={data.wordpressPage.excerpt}
     />
+    <NarrowTemplate>
     <h1>{data.wordpressPage.title}</h1>
     <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
+    </NarrowTemplate>
   </Layout>
 )
 export default PageTemplate
