595691845fda9c336c9bc62201861668edbc716f
Jason Vanderheyden <jason.vanderheyden@yahoo.com>
2019-12-17 12:24:14 -0500

    contact css

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,10 @@ import Layout from "../components/layout.js"
 import NarrowTemplate from "../components/NarrowTemplate"
 import GoogleMap from "../components/GoogleMap"
 import SEO from "../components/seo"
-import { FaBeer } from 'react-icons/fa'
+import { FaMapMarkerAlt } from 'react-icons/fa'
+import { FaEnvelope } from 'react-icons/fa'
+import { FaPhone } from 'react-icons/fa'
+import './contact.css'
 
 export default () => (
     <Layout>
@@ -13,16 +16,33 @@ export default () => (
       keywords={[`Architecture`, `Contact`, `Map`]}
     />
     <NarrowTemplate>
-    <h1>Contact Us</h1>
-    <div className="contact-info">
-        <h3> Lets go for a <FaBeer />? </h3>
-    </div>
-    <div className="contact-map">
-        <GoogleMap />
-    </div>
-    <div className="contact-form">
+      <div className="contact-box">
+        <div className="contact-left">
+          <div className="contact-map">
+            <GoogleMap />
+          </div>
+        </div>
+        <div className="contact-right">
+          <h1>Contact Us</h1>
+          <div className="contact-info">
+            <div className="contact-card">
+              <FaMapMarkerAlt />
+              <h3>123 Address St.<br/>City, St 12345</h3>
+            </div>
+            <div className="contact-card">
+              <FaEnvelope />
+              <h3>demo@email.com</h3>
+            </div>
+            <div className="contact-card">
+              <FaPhone />
+              <h3>(123) 456-7890</h3>
+            </div>
+          </div>
+          <div className="contact-form">
 
-    </div>
+          </div>
+        </div>
+      </div>
     </NarrowTemplate>
   </Layout>
 )
